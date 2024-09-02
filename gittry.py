0a737cc1014de94549f36f129c4ca9dce7ef2822
GFFB0314 <gbetnkom.bechir@gmail.com>
2024-09-02 15:36:28 +0100

    Counter flexibility

--- gittry.py
+++ gittry.py
@@ -1,19 +1,31 @@
 from random import randint
 
 def generate(max_value):
+    count = 0
     answ = randint(1, max_value)
     while True:
         try:
+            count += 1
             choice = int(input(f"Guess a number between 1-{max_value}: "))
             if choice == answ:
-                print("Congratulation your guess was wright!")
-                break
+                print("Your guess was right!")
+                if decision(count):
+                    pass
+                else:
+                    break
             elif choice < answ:
                 print("Your guess is too low!")
             elif choice > answ:
                 print("Your guess is too high!")
         except ValueError:
-            print("Your guess was be correct number!")
+            print("Your guess has to be a number!")
+
+
+def decision(count):
+    if count <= 3:
+        print("You won the game!")
+    else:
+        print("But you loose the game!")
 
 def main():
     max = int(input("Enter the max value to guess to: "))
